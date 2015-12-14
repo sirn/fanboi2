f80922071ee2251c9c9da3b6242b307eec8b893f
Kridsada Thanabulpong <sirn@ogsite.net>
2015-12-15 03:46:36 +0700

    Add date formatter class for formatting date into string.
    This is done in the same format as the rendered page which is done
    in format of "%b %d, %Y at %H:%M:%S". Timezone handling is not
    being done at all as we will rely on timezone returned by API.

---
+++ assets/app/javascripts/utils/date_formatter.ts
@@ -0,0 +1,34 @@
+const monthNames = [
+    'Jan',
+    'Feb',
+    'Mar',
+    'Apr',
+    'May',
+    'Jun',
+    'Jul',
+    'Aug',
+    'Sep',
+    'Oct',
+    'Nov',
+    'Dec',
+];
+
+export default class DateFormatter {
+    date: Date;
+
+    constructor(date: Date) {
+        this.date = date;
+    }
+
+    public toString(): string {
+        let year = this.date.getFullYear();
+        let month = monthNames[this.date.getMonth()];
+        let date = `00${this.date.getDate()}`.slice(-2);
+        let hour = `00${this.date.getHours()}`.slice(-2);
+        let minute = `00${this.date.getMinutes()}`.slice(-2);
+        let second = `00${this.date.getSeconds()}`.slice(-2);
+        let dateFormatted = `${month} ${date}, ${year}`;
+        let timeFormatted = `${hour}:${minute}:${second}`;
+        return `${dateFormatted} at ${timeFormatted}`;
+    }
+}
