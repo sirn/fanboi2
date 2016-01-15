2a230d623f1be99de04010c75a57099d103ece31
Kridsada Thanabulpong <sirn@ogsite.net>
2016-01-16 03:37:37 +0700

    Semantic cleanups for TypeScript files.

--- assets/app/javascripts/components/inline_quote.ts
+++ assets/app/javascripts/components/inline_quote.ts
@@ -1,4 +1,5 @@
 /// <reference path="../typings/dom4/dom4.d.ts" />
+/// <reference path="../typings/virtual-dom/virtual-dom.d.ts" />
 
 import vdom = require('virtual-dom');
 import dateFormatter = require('../utils/date_formatter');
